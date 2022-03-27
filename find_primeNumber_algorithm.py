0ed0e21f80b9e66e951ba735d080c83cb6b99bab
96Jerry <98335005+96Jerry@users.noreply.github.com>
2022-03-27 18:28:31 +0900

    Update speed of method
    아래 식이 위의 식보다 판별 속도가 빠르다.
    위의 식은 모든 소수를 찾을 때 속도가 빠르다.

--- find_primeNumber_algorithm.py
+++ find_primeNumber_algorithm.py
@@ -1,19 +1,33 @@
+# a = 100
+
+# answer=[]
+
+# x = [1]*(a+1)
+# x[1] = 0
+
+# for i in range(2,a):
+#     if x[i] == 1:
+#         j = 2
+#         while i*j<=a:
+#             x[i * j] = 0
+#             j += 1
+
+# for i in range(2,a+1):
+#     if x[i] == 1:
+#         answer.append(i)
+
+# print(answer)
+
 a = 100
+answer = []
 
-answer=[]
-
-x = [1]*(a+1)
-x[1] = 0
-
-for i in range(2,a):
-    if x[i] == 1:
-        j = 2
-        while i*j<=a:
-            x[i * j] = 0
-            j += 1
-
-for i in range(2,a+1):
-    if x[i] == 1:
-        answer.append(i)
-
-print(answer)
+def isPrime(a):
+    if a <= 3:
+        if a == 2 or a == 3:
+            return True
+        else:
+            return False
+    for i in range(2,int(a**0.5)+1):
+        if a % i == 0:
+            return False
+    return True
